e037114bf7fdddb3ebe39e45354a5389d07ac739
Dirk <raichu_dca@yahoo,.com>
2014-04-24 20:09:42 -0600

    Adding an Item Class

---
+++ 2530 Final Project - Rougelike/2530 Final Project - Rougelike/ItemClasses/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2530_Final_Project___Rougelike.ItemClasses
+{
+    class Item
+    {
+        String Name { get; private set; }
+        int AttackModifier { get; private set; }
+        int DeffenceModifier { get; private set; }
+        int HealthModifier { get; private set; }
+
+        public Item(String name, int attackMod, int deffenceMod, int healthMod)
+        {
+            Name = name;
+            AttackModifier = attackMod;
+            DeffenceModifier = deffenceMod;
+            HealthModifier = healthMod;
+        }
+    }
+}
