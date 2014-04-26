fe58063d50ba4ecfc2c486592680b3b401f385ba
Dirk <raichu_dca@yahoo,.com>
2014-04-26 16:35:33 -0600

    Added the Corndog and started the Abandoned house

---
+++ 2530 Final Project - Rougelike/2530 Final Project - Rougelike/ItemClasses/CornDog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2530_Final_Project___Rougelike.ItemClasses
+{
+    class CornDog : Item
+    {
+        public CornDog() : base("Corndog", 0, 9001, 0, 5, 'd', ConsoleColor.Yellow);
+    }
+}
---
+++ 2530 Final Project - Rougelike/2530 Final Project - Rougelike/mapClasses/MapAbandonedHouse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2530_Final_Project___Rougelike.mapClasses
+{
+    class MapAbandonedHouse : Map
+    {
+          public MapAbandonedHouse(int ep) : base(ep)
+        {
+            FileName = "mapAbandonedHouse.csv";
+
+            ReadMap();
+            AddTiles();
+            StandableTiles.AddRange(new List<int> { 1019 });
+
+        }
+
+
+
+    }
+}
