c355f755c0365daf5d1c0f5218118ca94024ac37
rbfullmer <pfullme4@bruinmail.slcc.edu>
2014-04-26 17:26:23 -0600

    More Monsters

---
+++ 2530 Final Project - Rougelike/2530 Final Project - Rougelike/characterClasses/Giant.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2530_Final_Project___Rougelike
+{
+    class Giant : Monster, NonPlayer
+    {   
+        public Giant(int x, int y) : base(x,y)
+        {
+            HP = 50;
+            XP = 50;
+
+            CharacterRepresentation = 'G';
+            Name = "Giant";
+            Color = ConsoleColor.Magenta;
+            Attack = 9;
+            Defense = 1;
+            Armor = 1;
+            MinDamage = 1;
+            MaxDamage = 10;
+        }
+
+        public void Move()
+        {
+        }
+
+        public override Character Interact(Character otherChar)
+        {
+            return this;
+        }
+
+        public override void DropItem()
+        {
+        }
+    }
+}
---
+++ 2530 Final Project - Rougelike/2530 Final Project - Rougelike/characterClasses/Troll.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2530_Final_Project___Rougelike
+{
+    class Troll : Monster, NonPlayer
+    {   
+        public Troll(int x, int y) : base(x,y)
+        {
+            HP = 25;
+            XP = 30;
+
+            CharacterRepresentation = 'T';
+            Name = "Troll";
+            Color = ConsoleColor.Gray;
+            Attack = 4;
+            Defense = 1;
+            Armor = 1;
+            MinDamage = 1;
+            MaxDamage = 5;
+        }
+
+        public void Move()
+        {
+        }
+
+        public override Character Interact(Character otherChar)
+        {
+            return this;
+        }
+
+        public override void DropItem()
+        {
+        }
+    }
+}
