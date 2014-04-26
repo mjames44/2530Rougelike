e29c2240b91f2f400c4edaab7b32deb684d74793
rbfullmer <pfullme4@bruinmail.slcc.edu>
2014-04-26 17:43:11 -0600

    Monsters all in

---
+++ 2530 Final Project - Rougelike/2530 Final Project - Rougelike/characterClasses/Bat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2530_Final_Project___Rougelike
+{
+    class TestMonster : Monster, NonPlayer
+    {   
+        public TestMonster(int x, int y) : base(x,y)
+        {
+            HP = 20;
+            XP = 500;
+
+            CharacterRepresentation = 'm';
+            Name = "Test Monster";
+            Color = ConsoleColor.Green;
+            Attack = 1;
+            Defense = 1;
+            Armor = 1;
+            MinDamage = 1;
+            MaxDamage = 1;
+        }
+
+        public void Move()
+        {
+        }
+
+        public override Character Interact(Character otherChar)
+        {
+            return this;
+        }
+
+        public override void DropItem()
+        {
+        }
+    }
+}
