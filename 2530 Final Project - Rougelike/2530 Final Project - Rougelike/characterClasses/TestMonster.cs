c35b0c76ac2063462210463af9968be7e720de57
mjames44 <mjames44@bruinmail.slcc.edu>
2014-04-26 16:16:07 -0600

    A Fix

--- 2530 Final Project - Rougelike/2530 Final Project - Rougelike/characterClasses/TestMonster.cs
+++ 2530 Final Project - Rougelike/2530 Final Project - Rougelike/characterClasses/TestMonster.cs
@@ -7,16 +7,7 @@ using System.Threading.Tasks;
 namespace _2530_Final_Project___Rougelike
 {
     class TestMonster : Monster, NonPlayer
-    {
-        char charRep = 'm';
-        string name = "Test Monster";
-        ConsoleColor color = ConsoleColor.Green;
-        int atk = 1; 
-        int def = 1; 
-        int arm = 1;
-        int min = 1;
-        int max = 1;
-        
+    {   
         public TestMonster(int x, int y) : base(x,y)
         {
             HP = 20;
