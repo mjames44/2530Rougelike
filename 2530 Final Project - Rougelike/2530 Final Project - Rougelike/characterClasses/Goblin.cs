028c801fa6ad77f128a657484d8dbbfd8615789e
rbfullmer <pfullme4@bruinmail.slcc.edu>
2014-04-26 17:06:38 -0600

    Goblin

---
+++ 2530 Final Project - Rougelike/2530 Final Project - Rougelike/characterClasses/Goblin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2530_Final_Project___Rougelike
+{
+    class Goblin : Monster, NonPlayer
+    {   
+        public Goblin(int x, int y) : base(x,y)
+        {
+            HP = 15;
+            XP = 10;
+
+            CharacterRepresentation = 'G';
+            Name = "Goblin";
+            Color = ConsoleColor.Green;
+            Attack = 2;
+            Defense = 1;
+            Armor = 1;
+            MinDamage = 1;
+            MaxDamage = 3;
+        }
+
+        public void Move()
+        {
+        }
+
+        public override Character Interact(Character otherChar)
+        {
+            return this;
+        }
+
+        public override void DropItem()
+        {
+        }
+    }
+}
