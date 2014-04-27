fce63cf1b56f7d58883dcf2989b1b49194e999a4
Dirk <raichu_dca@yahoo,.com>
2014-04-26 18:46:29 -0600

    Items have been made

---
+++ 2530 Final Project - Rougelike/2530 Final Project - Rougelike/ItemClasses/HealthPotion++.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2530_Final_Project___Rougelike.ItemClasses
+{
+    class HealthPotion__ : Item
+    {
+        public HealthPotion__() : base("Health Potion++", -10, -10, -10, 80, 'P', ConsoleColor.DarkMagenta) { }
+    }
+}
