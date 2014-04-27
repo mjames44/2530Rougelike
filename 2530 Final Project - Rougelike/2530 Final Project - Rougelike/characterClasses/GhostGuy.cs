431ec437f92e3d56a3af4f6672b4c66279a53774
Dirk <raichu_dca@yahoo,.com>
2014-04-27 01:57:05 -0600

    Ghost guy part 3

--- 2530 Final Project - Rougelike/2530 Final Project - Rougelike/characterClasses/GhostGuy.cs
+++ 2530 Final Project - Rougelike/2530 Final Project - Rougelike/characterClasses/GhostGuy.cs
@@ -16,16 +16,24 @@ namespace _2530_Final_Project___Rougelike
             Name = "Old Man";
             Color = ConsoleColor.Gray;
             talkCount = 0;
-            SpeechArray = new string[] { "It is Dangerous to go alone take this. *gives Stevo a Corndog*", "Good Luck" };
+            SpeechArray = new string[] { "It is Dangerous to go alone take this. *gives stevo a corndog", "Good Luck", "All right you can have this too.*gives stevo fruitcake" };
         }
 
         public override void Talk()
         {
             if (talkCount == 0)
             {
-                Game.Message = String.Format("{0}: {1}", Name, SpeechArray[0]);
-                Game.ShowMessage(0);
                 Game.AddItem(new CornDog());
+                Game.Message = String.Format("{0}: {1}\n", Name, SpeechArray[0]);
+                Game.ShowMessage(0);
+            }
+            else if (talkCount == 10)
+            {
+                Game.AddItem(new FruitCake());
+                Game.Message = String.Format("{0}: {1}\n", Name, SpeechArray[2]);
+                Game.ShowMessage(0);
+                
+                
             }
             else
             {
