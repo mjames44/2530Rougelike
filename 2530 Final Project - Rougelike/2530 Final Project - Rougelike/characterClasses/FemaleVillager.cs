bc12da502b2f12aa0a03976cb8cd5f012cd0840a
mjames44 <mjames44@bruinmail.slcc.edu>
2014-04-27 18:54:41 -0600

    apparently some changes

---
+++ 2530 Final Project - Rougelike/2530 Final Project - Rougelike/characterClasses/FemaleVillager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2530_Final_Project___Rougelike
+{
+    class FemaleVillager : Villager, NonPlayer
+    {
+        public FemaleVillager(int x, int y, string name)
+            : base(x, y)
+        {
+            CharacterRepresentation = name[0];
+            Name = name;
+
+            SpeechArray = new string[]{
+                "Hello.", 
+                "Oh our poor Princess.",
+                "Welcome to our town."};
+        }
+
+    }
+}
---
+++ 2530 Final Project - Rougelike/2530 Final Project - Rougelike/characterClasses/Horse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2530_Final_Project___Rougelike
+{
+    class Horse : NonPlayerCharacter, NonPlayer
+    {
+        public Horse(int x, int y)
+            : base(x, y)
+        {
+            CharacterRepresentation = 'H';
+            Name = "Horse";
+            Color = ConsoleColor.Gray;
+
+            SpeechArray = new string[] { "Neigh!", "*Stomps foot*" };
+        }
+
+        public int HP { get; set; }
+    }
+}
